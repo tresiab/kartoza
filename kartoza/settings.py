f758023a91bc7785f1ce35caf1f5161baa9c4c14
Tresia Burger <tresiab@gmail.com>
2025-11-04 20:38:41 +0200

    Add CI/CD pipeline

--- kartoza/settings.py
+++ kartoza/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,11 +78,11 @@ WSGI_APPLICATION = "kartoza.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "kartoza",
-        "USER": "kartoza",
-        "PASSWORD": "pass",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "NAME": os.environ.get("POSTGRES_DB", "kartoza"),
+        "USER": os.environ.get("POSTGRES_USER", "kartoza"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "pass"),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
 
