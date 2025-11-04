66cae10b79455df974bcd161f24083548f59b16a
Tresia Burger <tresiab@gmail.com>
2025-11-04 15:41:09 +0200

    Create KartozaAdminSite to replace default admin; sets site_header, site_title, and global "View site" link

--- kartoza/urls.py
+++ kartoza/urls.py
@@ -15,14 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
+from users.admin import admin_site
 from users.views import register_view
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin/", admin_site.urls),
     path("users/", include("users.urls")),
     path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page="/login/"), name="logout"),
